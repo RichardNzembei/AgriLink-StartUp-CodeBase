ad21df3b34e57874f61f5a697de4d3c29e70756c
RichardNzembei <richardsonreuben78@gmail.com>
2025-03-10 16:42:36 +0300

    update featuredProducts.vue

--- Frontend/components/Product/featuredProducts.vue
+++ Frontend/components/Product/featuredProducts.vue
@@ -5,7 +5,7 @@
         <ULink to="/Farmer/products" class="text-green-500 text-sm">See All</ULink>
       </div>
       <div class="grid grid-cols-2 gap-4">
-        <UCard v-for="(product, index) in productStore.products" :key="index" class="text-center">
+        <UCard v-for="(product, index) in productStore.products" :style="{backgroundColor: 'white'}" :key="index" class="text-center">
           <img :src="product.imageUrl" alt="Product Image" class="w-full h-40 object-cover rounded-t-lg" />
           <div class="p-2">
             <h3 class="text-sm font-semibold">{{ product.name }}</h3>
@@ -17,7 +17,7 @@
   </template>
   
   <script setup>
-  import { onMounted } from "vue";
+import { onMounted } from "vue";
   import { useProductStore } from "~/store/useProductStore";
   
   const productStore = useProductStore();
