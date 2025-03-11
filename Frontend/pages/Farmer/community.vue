5a83000efba827a96ddf06c3ae170ec9da12f85d
RichardNzembei <richardsonreuben78@gmail.com>
2025-03-11 21:22:05 +0300

    update community.vue

--- Frontend/pages/Farmer/community.vue
+++ Frontend/pages/Farmer/community.vue
@@ -52,7 +52,7 @@
 
     <div v-if="post.comments?.length" class="space-y-2">
         <div v-for="comment in post.comments" :key="comment.id" class="bg-gray-100 p-2 rounded-lg">
-            <strong class="text-green-700">{{ comment.author }}:</strong> <span class="text-grey-900">{{ comment.content }}</span>
+            <strong class="text-green-700">{{ comment.author }}:</strong> <span class="text-black">{{ comment.content }}</span>
         </div>
     </div>
     <p v-else class="text-gray-400">No comments yet.</p>
