5f84f2f0279826a6cc5fe03761b27bdcb85bb7cf
RichardNzembei <richardsonreuben78@gmail.com>
2025-03-17 23:15:26 +0300

    update displays.vue and displays.vue

--- Frontend/components/App/Buyer/displays.vue
+++ Frontend/components/App/Buyer/displays.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 p-8 justify-center items-center">
+    <div class=" sm:grid-cols-2 lg:grid-cols-4 gap-8 p-8 justify-center items-center">
       
       <!-- Farmers Card -->
       <NuxtLink to="/Buyer/farmers">
